59c7b6f29331e1b6ab4f9aa670c5aa6248df18e1
lolashot <ololadeshot@gmail.com>
2022-10-06 09:11:16 +0100

    new project

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,19 +1,5 @@
 const tasks = [
-  {
-    id: 1,
-    description: 'Clean Room',
-    completed: 'false',
-  },
-  {
-    id: 2,
-    description: 'Start React Project',
-    completed: 'false',
-  },
-  {
-    id: 3,
-    description: 'Visit Eben',
-    completed: 'false',
-  },
+ 
 ];
 
 export default tasks;
