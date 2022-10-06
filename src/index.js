99de66e6858372df2108997fdbd3d9990000b686
lolashot <ololadeshot@gmail.com>
2022-10-06 20:05:36 +0100

    update all files

--- src/index.js
+++ src/index.js
@@ -4,3 +4,38 @@ import AllTasks from './modules/allTasks.js';
 document.addEventListener('DOMContentLoaded', () => {
   AllTasks.displayTasks();
 });
+import './style.css';
+import EventObj, { data, input } from './modules/addEvent.js';
+
+const press = document.querySelector('#enter');
+const done = document.querySelector('.clear-done');
+
+// Render tasks to DOM
+window.onload = EventObj.createEvent();
+
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    EventObj.addList();
+    EventObj.createEvent();
+    input.value = '';
+  }
+});
+
+press.addEventListener('click', () => {
+  EventObj.addList();
+  EventObj.createEvent();
+  input.value = '';
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('to-do')) {
+    e.target.parentElement.classList.add('#ff0');
+  }
+});
+
+done.addEventListener('click', () => {
+  const filterd = data.filter((x) => x.complete !== true);
+  localStorage.setItem('data', JSON.stringify(filterd));
+  EventObj.createEvent();
+  window.location.reload();
+});
